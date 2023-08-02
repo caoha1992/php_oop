e178459365144dd92e93f577dda8d7370ad0afc4
caoha1992 <141201204+caoha1992@users.noreply.github.com>
2023-08-02 10:20:55 +0700

    Add files via upload

---
+++ Http/Form/LoginForm.php
@@ -0,0 +1,19 @@
+<?php
+namespace Http\Form;
+use Core\Auth;
+use Core\Validate;
+class LoginForm
+{
+    public static $error = [];
+    public static function checkLogin($email,$password){
+        if(! Validate::string($password)){
+            static::$error['password'] = 'Password No Valid';
+        }
+        if (! Validate::email($email)){
+             static::$error['email'] = 'Email No Valid';
+        }
+    }
+    public static function error(){
+        return static::$error;
+    }
+}
---
+++ Http/controllers/session/store.php
@@ -0,0 +1,15 @@
+<?php
+use Core\Auth;
+use Core\Session;
+use Http\Form\LoginForm;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+if (! LoginForm::error()) {
+    if (Auth::atempt($email, $password) === null) {
+        redirect('/');
+    }
+}
+Session::flash('old',$email);
+Session::flash('error', Auth::atempt($email, $password));
+redirect('/login');
